a97826cc2e7f08392c2e671943db145ab17b1785
cchura94 <cchura.cpc@gmail.com>
2023-06-08 21:00:43 -0400

    crud categorias

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -1,9 +1,19 @@
 import models from "./../database/models/index"
+import { Op } from "sequelize"
 
 export default {
     listar: async (req, res) => {
         try{
-            const categorias = await models.Categoria.findAll();
+            // /api/categoria?q=rop
+            const buscar = req.query.q?req.query.q:'';
+
+            const categorias = await models.Categoria.findAll({
+                where: {
+                    nombre: {
+                        [Op.like]: `%${buscar}%`
+                    }
+                }
+            });
             return res.status(200).json(categorias);
         }catch(error){
             return res.status(500).json({message: error.message})
@@ -25,12 +35,51 @@ export default {
         }
     },
     mostrar: async (req, res) => {
-        
+        try {
+            const id = req.params.id;
+            const categoria = await models.Categoria.findByPk(id);
+            if(categoria){
+                return res.status(200).json(categoria)
+            }else{
+                return res.status(404).json({message: "Categoria no existe"});
+            }
+            
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        }        
     },
     modificar: async (req, res) => {
-        
+        try {
+            const id = req.params.id;
+            const { nombre, detalle } = req.body;
+            const categoria = await models.Categoria.findByPk(id);
+            if(categoria){
+                await models.Categoria.update({nombre, detalle}, {
+                    where: {
+                        id: categoria.id
+                    }
+                })
+                return res.status(200).json({message: "Categoria Actualizada"})
+            }else{
+                return res.status(404).json({message: "Categoria no existe"});
+            }
+            
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        } 
     },
     eliminar: async (req, res) => {
-        
+        try {
+            const id = req.params.id;
+            
+            await models.Categoria.destroy({
+                where: {
+                    id: id
+                }
+            })
+            return res.status(200).json({message: "Categoria Eliminada"})
+        } catch (error) {
+            return res.status(500).json({message: error.message})
+        } 
     }
 }
